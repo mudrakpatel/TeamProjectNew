f3b3b5a61796f6de8a4a091249d514ffc5d6efd7
mudrakpatel <mudrakpatel96@gmail.com>
2016-10-26 03:54:18 -0500

    Added login logic

--- TeamProjectNew/Login.aspx.cs
+++ TeamProjectNew/Login.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+//required using statements
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 
 namespace TeamProjectNew
 {
@@ -16,6 +20,28 @@ namespace TeamProjectNew
 
         protected void LoginButton_Click(object sender, EventArgs e)
         {
+            //create a new userStore and userManager objects
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            //search for and create a new user object
+            var user = userManager.Find(UserNameTextBox.Text, PasswordTextBox.Text);
+
+            //if a match is found for the user
+            if (user!=null) {
+                //authenticate and login the user
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                //signin the user
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false}, userIdentity);
+
+                //redirect the user to the Default.aspx page
+                Response.Redirect("~/Default.aspx");
+            } else {
+                StatusLabel.Text = "Invalid Username or Password!";
+                AlertFlash.Visible = true;
+            }
 
         }
     }
